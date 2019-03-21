d9171c6cd6b811b46f276f0413a867b2c2b14e15
michael-rai <michaelrai.prgmr@gmail.com>
2019-03-21 02:16:54 -0400

    completed hello there

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new Center(
+      child: new Text(
+        "hello, there",
+        textDirection: TextDirection.ltr,
+      )
+    )
+  );
+}
